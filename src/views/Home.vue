7f5085d28f0984f77f700a317ee75fc4c8ba9758
Daltimore <obinnaleo78@gmail.com>
2020-01-07 12:16:59 +0100

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,80 @@
+<template>
+  <main id="first-theme">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 d-flex">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+          <div class="avatar-section">
+            <div class="row">
+              <div class="col-lg-3 avatar">
+                <img src="@/assets/img/avatar.png" alt="">
+              </div>
+              <div class="col-lg-6 user-details">
+                <h4>Conor</h4>
+                <p>Ui/Ux designer</p>
+                <div class="d-flex justify-content-between">
+                  <div class="rate">
+                    <span>&#x24;10</span>
+                    <p>Hourly Rate</p>
+                  </div>
+                  <div class="rate">
+                    <span>35 hours</span>
+                    <p>Weekly Availability</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 audio-section">
+                <div class="interv__container">
+                  <p>interviews</p>
+                  <div class="interv">
+                    <span><i class="fas fa-volume-up"></i></span>
+                    <span><i class="fas fa-volume-up"></i></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="row tap-section">
+              <div class="col-lg-6">
+                <button class="btn btn-sm">Tap to chat</button>
+              </div>
+              <div class="col-lg-6 icon-container">
+                <div>
+                  <i class="fab fa-behance"></i>
+                </div>
+                <div>
+                  <i class="fab fa-dribbble"></i>
+                </div>
+                <div>
+                  <i class="fab fa-instagram"></i>
+                </div>
+                <div>
+                  <i class="fab fa-linkedin-in"></i>
+                </div>
+                <div>
+                  <img src="@/assets/img/google.png" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="d-flex justify-content-center align-item-center hire-me">
+              <button class="btn btn-md">Hire me</button>
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 purple-section">
+          my contetnt
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+
+</style>
